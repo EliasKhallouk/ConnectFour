d78ab50f31e49cddf49996dd02d0d1942e5b3074
Elias Khallouk <eliaskhallouk@gmail.com>
2024-03-15 22:23:08 +0100

    Page Number Player + Page Time of Game + Page UserName and Chosse Color

--- mini.cpp
+++ mini.cpp
@@ -5,63 +5,154 @@
 #include <QVBoxLayout>
 #include <QHBoxLayout>
 #include <QDesktopWidget>
+#include <QStackedWidget>
+#include <QComboBox>
+#include <QColorDialog>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    // Création du titre
-    QLabel titleLabel("Choisissez votre pseudo");
-    QFont titleFont = titleLabel.font();
-    titleFont.setPointSize(24);
-    titleLabel.setFont(titleFont);
+    // MISE EN PAGE NOMBRE JOUEUR
+    QVBoxLayout layoutNumberJoueur;
+        // Création du titre
+        QLabel titleNumberJoueur("Choisissez le nombre de joueur");
+        QFont titleFont = titleNumberJoueur.font();
+        titleFont.setPointSize(24);
+        titleNumberJoueur.setFont(titleFont);
+        titleNumberJoueur.setStyleSheet("color: #FFFFFF;");
 
-    // Création du champ d'entrée
-    QLineEdit inputPseudo;
-    inputPseudo.setFixedWidth(200);
-    inputPseudo.setStyleSheet("border: 1px solid white; border-radius: 50px;");
+        // Création du champ d'entrée
+        QLineEdit inputNumberJoueur;
+        inputNumberJoueur.setFixedWidth(200);
+        inputNumberJoueur.setStyleSheet("border: 1px solid white; border-radius: 50px; color: #FFFFFF");
 
-    // Création du bouton
-    QPushButton suivantPseudo("Suivant");
-    suivantPseudo.setFixedWidth(200);
-    suivantPseudo.setStyleSheet("border: 1px solid white; border-radius: 50px;");
+        // Création du bouton
+        QPushButton suivantNumberJoueur("Suivant");
+        suivantNumberJoueur.setFixedWidth(200);
+        suivantNumberJoueur.setStyleSheet("border: 1px solid white; border-radius: 50px; background-color: #FCA311");
 
-    // Mise en page principale
-    QVBoxLayout mainLayout;
+        layoutNumberJoueur.addWidget(&titleNumberJoueur, 0, Qt::AlignVCenter | Qt::AlignHCenter);
+        QHBoxLayout centerLayoutNumberJoueur;
+        centerLayoutNumberJoueur.addWidget(&inputNumberJoueur, 0, Qt::AlignHCenter | Qt::AlignVCenter);
+        centerLayoutNumberJoueur.addWidget(&suivantNumberJoueur, 0, Qt::AlignHCenter | Qt::AlignVCenter);
+        layoutNumberJoueur.addLayout(&centerLayoutNumberJoueur);
 
-    // Ajout du titre centré verticalement
-    titleLabel.setStyleSheet("color: #FFFFFF;");  // Définir la couleur du texte
-    mainLayout.addWidget(&titleLabel, 0, Qt::AlignVCenter | Qt::AlignHCenter);
+        // Création du widget principal pour la page de choix du nombre de joueurs
+        QWidget pageNumberJoueur;
+        pageNumberJoueur.setLayout(&layoutNumberJoueur);
+        pageNumberJoueur.setStyleSheet("background-color: #14213D;");
 
-    // Création de la mise en page horizontale pour centrer les éléments
-    QHBoxLayout centerLayout;
-    centerLayout.addWidget(&inputPseudo, 0, Qt::AlignHCenter | Qt::AlignVCenter);
 
-    // Définir la couleur de fond du bouton
-    suivantPseudo.setStyleSheet("background-color: #FCA311");  // Définir la couleur du fond et du texte
-    centerLayout.addWidget(&suivantPseudo, 0, Qt::AlignHCenter | Qt::AlignVCenter);
 
-    // Ajout de la mise en page horizontale à la mise en page principale
-    mainLayout.addLayout(&centerLayout);
+    // MISE EN PAGE TEMPS DE PARTIE
+    QVBoxLayout layoutTempPartie;
+        // Création du titre
+        QLabel titleTempPartie("Choisissez le temps de partie");
+        titleFont = titleTempPartie.font();
+        titleFont.setPointSize(24);
+        titleTempPartie.setFont(titleFont);
+        titleTempPartie.setStyleSheet("color: #FFFFFF;");
 
-    // Définir la taille de la fenêtre principale à 700x700
-    QWidget mainWidget;
-    mainWidget.setLayout(&mainLayout);
+        // Création du champ d'entrée
+        QComboBox comboBoxTempPartie;
+            comboBoxTempPartie.setFixedWidth(200);
+            comboBoxTempPartie.addItem("3 minutes");
+            comboBoxTempPartie.addItem("5 minutes");
+            comboBoxTempPartie.addItem("10 minutes");
+            comboBoxTempPartie.setStyleSheet("border: 1px solid white; border-radius: 50px; color: #FFFFFF");
 
-    // Définir la couleur de fond de la fenêtre principale
-    mainWidget.setStyleSheet("background-color: #14213D;");
+        // Création du bouton
+        QPushButton suivantTempPartie("Suivant");
+        suivantTempPartie.setFixedWidth(200);
+        suivantTempPartie.setStyleSheet("border: 1px solid white; border-radius: 50px; background-color: #FCA311");
 
-    mainWidget.resize(700, 700);
-    mainWidget.setWindowTitle("Connect Four");
+        layoutTempPartie.addWidget(&titleTempPartie, 0, Qt::AlignVCenter | Qt::AlignHCenter);
+        QHBoxLayout centerLayoutTempPartie;
+        centerLayoutTempPartie.addWidget(&comboBoxTempPartie, 0, Qt::AlignHCenter | Qt::AlignVCenter);
+        centerLayoutTempPartie.addWidget(&suivantTempPartie, 0, Qt::AlignHCenter | Qt::AlignVCenter);
+        layoutTempPartie.addLayout(&centerLayoutTempPartie);
+
+        // Création du widget principal pour la page de choix du temps de partie
+        QWidget pageTempPartie;
+        pageTempPartie.setLayout(&layoutTempPartie);
+        pageTempPartie.setStyleSheet("background-color: #14213D;");
+
+
+
+    // MISE EN PAGE PSEUDO
+    QVBoxLayout layoutPseudo;
+        // Création du titre
+        QLabel titlePseudo("Choisissez le pseudo et la couleur");
+        titleFont = titlePseudo.font();
+        titleFont.setPointSize(24);
+        titlePseudo.setFont(titleFont);
+        titlePseudo.setStyleSheet("color: #FFFFFF;");
+
+        // Création du champ d'entrée pour le pseudo
+        QLineEdit inputPseudo;
+        inputPseudo.setFixedWidth(200);
+        inputPseudo.setStyleSheet("border: 1px solid white; border-radius: 50px; color: #FFFFFF");
+
+        // Création du bouton pour choisir la couleur
+        QPushButton chooseColorButton("Choisir une couleur");
+        chooseColorButton.setFixedWidth(200);
+        chooseColorButton.setStyleSheet("border: 1px solid white; border-radius: 50px; background-color: #FCA311");
+
+        // Création du bouton "Suivant"
+        QPushButton suivantPseudo("Suivant");
+        suivantPseudo.setFixedWidth(200);
+        suivantPseudo.setStyleSheet("border: 1px solid white; border-radius: 50px; background-color: #FCA311");
+
+        layoutPseudo.addWidget(&titlePseudo, 0, Qt::AlignVCenter | Qt::AlignHCenter);
+        QHBoxLayout centerLayoutPseudo;
+        centerLayoutPseudo.addWidget(&inputPseudo, 0, Qt::AlignHCenter | Qt::AlignVCenter);
+        centerLayoutPseudo.addWidget(&chooseColorButton, 0, Qt::AlignHCenter | Qt::AlignVCenter); // Ajout du bouton pour choisir la couleur
+        centerLayoutPseudo.addWidget(&suivantPseudo, 0, Qt::AlignHCenter | Qt::AlignVCenter);
+        layoutPseudo.addLayout(&centerLayoutPseudo);
+
+        // Création du widget principal pour la page de choix du nombre de joueurs
+        QWidget pagePseudo;
+        pagePseudo.setLayout(&layoutPseudo);
+        pagePseudo.setStyleSheet("background-color: #14213D;");
+
+    // Création d'un QStackedWidget pour gérer les pages
+    QStackedWidget stackedWidget;
+    stackedWidget.addWidget(&pageNumberJoueur);  // Ajout de la page principale
+    stackedWidget.addWidget(&pageTempPartie);    // Ajout de la deuxième page
+    stackedWidget.addWidget(&pagePseudo);        // Ajout de la troisieme page
+    stackedWidget.setCurrentWidget(&pageNumberJoueur);  // Définir la page principale comme étant celle affichée initialement
+    stackedWidget.resize(700, 700);
+    stackedWidget.setWindowTitle("Connect Four");
+
+    // Connecter le clic sur le bouton "Suivant" pour passer à la nouvelle page
+    QObject::connect(&suivantNumberJoueur, &QPushButton::clicked, [&stackedWidget]() {
+        stackedWidget.setCurrentIndex(1);  // Passer à la deuxième page
+    });
+
+    // Connecter le clic sur le bouton "Suivant" pour passer à la pagePseudo
+    QObject::connect(&suivantTempPartie, &QPushButton::clicked, [&stackedWidget]() {
+        stackedWidget.setCurrentIndex(2);  // Passer à la troisième page (index 2)
+    });
+
+    // Connecter le clic sur le bouton pour choisir la couleur
+    QObject::connect(&chooseColorButton, &QPushButton::clicked, [&inputPseudo, &pagePseudo]() {
+        QColor color = QColorDialog::getColor(Qt::white, &pagePseudo, "Choisissez une couleur");
+        if (color.isValid()) {
+            // Mettre à jour la couleur du champ d'entrée du pseudo
+            QString styleSheet = QString("border: 1px solid white; border-radius: 50px; color: %1").arg(color.name());
+            inputPseudo.setStyleSheet(styleSheet);
+        }
+    });
 
     // Centrer la fenêtre sur l'écran
     QRect mainScreenSize = QApplication::desktop()->screenGeometry();
-    int x = (mainScreenSize.width() - mainWidget.width()) / 2;
-    int y = (mainScreenSize.height() - mainWidget.height()) / 2;
-    mainWidget.move(x, y);
+    int x = (mainScreenSize.width() - stackedWidget.width()) / 2;
+    int y = (mainScreenSize.height() - stackedWidget.height()) / 2;
+    stackedWidget.move(x, y);
 
-    // Affichage du widget principal
-    mainWidget.show();
+    // Afficher le widget principal
+    stackedWidget.show();
 
     return app.exec();
 }
